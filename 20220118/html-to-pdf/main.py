861938e63c252d6661755a0d069b7da0465f4038
Joowan Kim <rlawndhks217@gmail.com>
2022-01-18 22:19:50 +0900

    Add html2pdf converter

--- 20220118/html-to-pdf/main.py
+++ 20220118/html-to-pdf/main.py
@@ -1,16 +1,14 @@
-# This is a sample Python script.
+import sys
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+import pdfkit
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+def main(html_path, pdf_path):
+    pdfkit.from_file(html_path, output_path=pdf_path)
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+if __name__ == "__main__":
+    html = sys.argv[1]
+    pdf = sys.argv[2]
+
+    main(html, pdf)
